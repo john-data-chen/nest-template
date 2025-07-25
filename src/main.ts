2189c5d4eac6298bb20425924b4006d190dcff08
JianAn Chen <john.data.chen@gmail.com>
2025-07-25 08:28:18 +0800

    refactor: export hard code port number into constants

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PORT } from './constants/app.constants';
 
 declare const module: any;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT);
 
   if (module.hot) {
     module.hot.accept();
